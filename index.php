c35fdba66dcec68d73cb8e5dee61afc4788ccc2a
sn5ake6 <sn5ake6@gmail.com>
2018-02-06 23:59:41 +0500

    update example of use

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
 <?php
 require_once 'autoload.php';
+require_once('./vendor/autoload.php');
 
 use Chess\Board;
 use Chess\BoardItem;
 use Chess\Position;
 use Chess\Factories\FigureFactory;
 use Chess\Storages\File;
+use Chess\Storages\Redis;
 
 $size = 8;
 $board = new Board($size, $size) ;
 $factory = new FigureFactory();
+$start_position = new Position(1, 1);
 $board->addItem(
-    new BoardItem(new Position(1, 1), $factory->createPawn('white'))
+    new BoardItem($start_position, $factory->createPawn('white'))
 );
 
-$storage = new File('storage.dat');
-$storage->save($board);
-$board->deleteItem(new Position(1, 1));
+$file_storage = new File('storage.dat');
+$file_storage->save($board);
 
-$other_board = $storage->load();
-$other_board->moveItem(new Position(1, 1), new Position(5, 3));
+$redis_storage = new Redis('board', new Predis\Client());
+$redis_storage->save($board);
+
+$board->deleteItem($start_position);
+
+$another_position = new Position(5, 3);
+$board_from_file = $file_storage->load();
+$board_from_file->moveItem($start_position, $another_position);
+
+$board_from_redis = $redis_storage->load();
+$board_from_redis->moveItem($start_position, $another_position);
